32acac41e9307cc6353ee360402a27854da45c24
ximvhs <92147920+ximvhs@users.noreply.github.com>
2021-11-12 21:31:37 +0700

    upload

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'nov 25, 2021',
+  birthdate: 'nov 11, 2021',
   name: 'Nguyễn Anh Thư'
 };
 
